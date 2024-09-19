8629f8240a7c10f64fd054b933f293f0b93a6ca3
Imen Attatra <imen.attatra@gmail.com>
2024-09-19 11:56:55 +0200

    update tests calling the previsouly renamed metrwidgets url

--- tests/handlers/test_metr_widgets.py
+++ tests/handlers/test_metr_widgets.py
@@ -1,7 +1,5 @@
 from redash import models
 from tests import BaseTestCase
-from tests.handlers.test_widgets import WidgetAPITest
-
 
 class TestMetrWidgetTagsResource(BaseTestCase):
     def test_post_updates_tags_of_metr_widget(self):
@@ -13,7 +11,7 @@ class TestMetrWidgetTagsResource(BaseTestCase):
         new_tags = ["tag3", "tag4"]
         widget_properties = {"tags": new_tags}
 
-        response =  self.make_request("post",f"/api/widgets/{widget.id}/tags", data=widget_properties)
+        response =  self.make_request("post",f"/api/metrwidgets/{widget.id}/tags", data=widget_properties)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(metr_widget.tags, new_tags)
@@ -28,20 +26,8 @@ class TestMetrWidgetTagsResource(BaseTestCase):
         new_tags = ["tag3", "tag4"]
         widget_properties = {"tags": new_tags}
 
-        response =  self.make_request("post",f"/api/widgets/{widget.id}/tags", data=widget_properties)
+        response =  self.make_request("post",f"/api/metrwidgets/{widget.id}/tags", data=widget_properties)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(widget.metr_widget[0].tags, new_tags)
         self.assertEqual(widget.metr_widget[0].widget_id,widget.id)
-
-class TestWidgetAPI(WidgetAPITest):
-    def test_create_widget_creates_also_metr_widget(self):
-        self.assertTrue(len(models.metrWidget.query.all())  == 0)
-
-        dashboard = self.factory.create_dashboard()
-        vis = self.factory.create_visualization()
-
-        rv = self.create_widget(dashboard, vis)
-
-        self.assertEqual(rv.status_code, 200)
-        self.assertTrue(len(models.metrWidget.query.all()) == 1)
