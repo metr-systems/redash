02f78f8b75c22336a133a403f10e7adbf08798f5
Imen Attatra <imen.attatra@gmail.com>
2024-10-22 19:07:27 +0200

    optimize  function

--- client/app/components/dashboards/utils.js
+++ client/app/components/dashboards/utils.js
@@ -5,54 +5,42 @@ export function keepLayoutsOrder(orderedLayoutsIds, layouts, widgets) {
   let currentY = 0;
   let usedColumns = 0;
   let previousLayout = null;
-  let heightLeft = 0;
   let biggestHeightInLine = 0;
+
   orderedLayoutsIds.forEach(id => {
     const layout = layouts.find(l => l.i === id.toString());
     if (layout) {
+      const foundWidget = widgets.find(widget => widget.id.toString() === layout.i);
+      const isWidgetTextbox = foundWidget && !foundWidget.visualization;
+
       if (!previousLayout) {
-        // first layout ever
+        // First layout
         layout.y = currentY;
         layout.x = 0;
         usedColumns = layout.w;
         biggestHeightInLine = layout.h;
       } else {
-        // we have a previous layout
-        // we check if previous layout is a textbox
-        let foundPreviousWidget = widgets.find(widget => widget.id.toString() === previousLayout.i);
-        let isPreviousTextbox = foundPreviousWidget && !foundPreviousWidget.visualization;
-        // we check if current layout is textbox
-        let foundWidget = widgets.find(widget => widget.id.toString() === layout.i);
-        let isWidgetTextbox = foundWidget && !foundWidget.visualization;
+        const foundPreviousWidget = widgets.find(widget => widget.id.toString() === previousLayout.i);
+        const isPreviousTextbox = foundPreviousWidget && !foundPreviousWidget.visualization;
 
-        // we check if we need to put the layout in new line
-        let newLineCondition =
+        const newLineCondition =
           isWidgetTextbox ||
           isPreviousTextbox ||
           previousLayout.w + layout.w > cfg.columns ||
           usedColumns + layout.w > cfg.columns;
 
         if (newLineCondition) {
-          // put the layout in new line
-          if (heightLeft > 0) {
-            currentY += heightLeft;
-          } else if (heightLeft < 0) {
-            currentY += biggestHeightInLine;
-          } else {
-            currentY += previousLayout.h;
-          }
-
+          // New line
+          currentY += biggestHeightInLine;
           layout.y = currentY;
           layout.x = 0;
           usedColumns = layout.w;
           biggestHeightInLine = layout.h;
         } else {
-          // put the layout in same line as previous
+          // Same line
           layout.y = previousLayout.y;
           layout.x = previousLayout.x === 0 ? previousLayout.w : 0;
-          heightLeft = layout.h - previousLayout.h;
-          biggestHeightInLine = Math.max(previousLayout.h, layout.h);
-          currentY += biggestHeightInLine - previousLayout.h;
+          biggestHeightInLine = Math.max(biggestHeightInLine, layout.h);
           usedColumns += layout.w;
         }
       }
