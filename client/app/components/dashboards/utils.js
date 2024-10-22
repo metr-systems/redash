242e5163831ef0b463b9c773443a11820bcdea8d
Imen Attatra <imen.attatra@gmail.com>
2024-10-22 21:13:23 +0200

    update ´keepLayoutsOrder´for better code

--- client/app/components/dashboards/utils.js
+++ client/app/components/dashboards/utils.js
@@ -4,49 +4,51 @@ export function keepLayoutsOrder(orderedLayoutsIds, layouts, widgets) {
   const newLayouts = [];
   let currentY = 0;
   let usedColumns = 0;
-  let previousLayout = null;
   let biggestHeightInLine = 0;
+
+  const getWidgetById = id => widgets.find(widget => widget.id.toString() === id.toString());
+  const isTextbox = widget => widget && !widget.visualization;
 
   orderedLayoutsIds.forEach(id => {
     const layout = layouts.find(l => l.i === id.toString());
-    if (layout) {
-      const foundWidget = widgets.find(widget => widget.id.toString() === layout.i);
-      const isWidgetTextbox = foundWidget && !foundWidget.visualization;
+    if (!layout) return;
 
-      if (!previousLayout) {
-        // First layout
+    const widget = getWidgetById(layout.i);
+    const isWidgetTextbox = isTextbox(widget);
+
+    if (newLayouts.length === 0) {
+      // Position the first layout
+      layout.y = currentY;
+      layout.x = 0;
+      usedColumns = layout.w;
+      biggestHeightInLine = layout.h;
+    } else {
+      const previousLayout = newLayouts[newLayouts.length - 1];
+      const previousWidget = getWidgetById(previousLayout.i);
+      const isPreviousTextbox = isTextbox(previousWidget);
+
+      const shouldMoveToNewLine =
+        isWidgetTextbox ||
+        isPreviousTextbox ||
+        previousLayout.w + layout.w > cfg.columns ||
+        usedColumns + layout.w > cfg.columns;
+
+      if (shouldMoveToNewLine) {
+        // Move to a new line
+        currentY += biggestHeightInLine;
         layout.y = currentY;
         layout.x = 0;
         usedColumns = layout.w;
         biggestHeightInLine = layout.h;
       } else {
-        const foundPreviousWidget = widgets.find(widget => widget.id.toString() === previousLayout.i);
-        const isPreviousTextbox = foundPreviousWidget && !foundPreviousWidget.visualization;
-
-        const newLineCondition =
-          isWidgetTextbox ||
-          isPreviousTextbox ||
-          previousLayout.w + layout.w > cfg.columns ||
-          usedColumns + layout.w > cfg.columns;
-
-        if (newLineCondition) {
-          // New line
-          currentY += biggestHeightInLine;
-          layout.y = currentY;
-          layout.x = 0;
-          usedColumns = layout.w;
-          biggestHeightInLine = layout.h;
-        } else {
-          // Same line
-          layout.y = previousLayout.y;
-          layout.x = previousLayout.x + previousLayout.w;
-          biggestHeightInLine = Math.max(biggestHeightInLine, layout.h);
-          usedColumns += layout.w;
-        }
+        // Place on the same line
+        layout.y = previousLayout.y;
+        layout.x = previousLayout.x + previousLayout.w;
+        biggestHeightInLine = Math.max(biggestHeightInLine, layout.h);
+        usedColumns += layout.w;
       }
-      newLayouts.push(layout);
-      previousLayout = layout;
     }
+    newLayouts.push(layout);
   });
 
   return newLayouts;
