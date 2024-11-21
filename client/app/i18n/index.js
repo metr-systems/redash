7660e8a740b0b12875bf8d3b0ff43b75c4e23fc9
Imen Attatra <imen.attatra@gmail.com>
2024-11-21 18:16:55 +0100

    First attempt to translate considering more than one namespace and many translation options

--- client/app/i18n/index.js
+++ client/app/i18n/index.js
@@ -1,18 +1,21 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import en from "/client/app/i18n/locales/en/common.json";
-import de from "/client/app/i18n/locales/de/common.json";
+import common_en from "/client/app/i18n/locales/en/common.json";
+import common_de from "/client/app/i18n/locales/de/common.json";
+
+import EmptyState_en from "/client/app/i18n/locales/en/EmptyState.json";
+import EmptyState_de from "/client/app/i18n/locales/de/EmptyState.json";
 
 i18n.use(initReactI18next).init({
   resources: {
-    en: { common: en },
-    de: { common: de },
+    en: { common: common_en, EmptyState: EmptyState_en },
+    de: { common: common_de, EmptyState: EmptyState_de },
   },
   lng: "de",
   fallbackLng: "de",
   preload: ["en", "de"],
-  ns: ["common"],
+  ns: ["common", "EmptyState"],
   defaultNS: "common",
   interpolation: {
     escapeValue: false,
--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -5,7 +5,5 @@ module.exports = {
   defaultNamespace: "common", // Default namespace
   keySeparator: false, // Set to false if keys in code are dot-separated
   nsSeparator: false, // Set to false if namespaces in code are dot-separated
-  defaultValue: "", // Default text to insert as translation
-  useKeysAsDefaultValue: false, // Set to true if keys should be default values
-  verbose: true // Output details to the console
+  useKeysAsDefaultValue: false // Set to true if keys should be default values
 };
