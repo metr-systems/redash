4bb57d62ccc947baee6b29f8efa79350768295c9
Imen Attatra <imen.attatra@gmail.com>
2024-11-21 11:19:15 +0100

    [Fix tests] mock i18next and react-i18next for tests

--- client/app/__tests__/mocks.js
+++ client/app/__tests__/mocks.js
@@ -3,3 +3,16 @@ import MockDate from "mockdate";
 const date = new Date("2000-01-01T02:00:00.000");
 
 MockDate.set(date);
+
+jest.mock("i18next", () => ({
+  t: key =>
+    key
+      .split(":")
+      .pop()
+      .trim(),
+  changeLanguage: jest.fn(),
+}));
+
+jest.mock("react-i18next", () => ({
+  Trans: ({ children }) => (typeof children === "function" ? children("mockedTranslation") : children || null),
+}));
