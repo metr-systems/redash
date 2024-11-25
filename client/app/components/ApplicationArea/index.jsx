37efe3eaef52d497b9938e639646266a1a10c2e8
Imen Attatra <imen.attatra@gmail.com>
2024-11-25 16:34:40 +0100

    do not translate console.error texts

--- client/app/components/ApplicationArea/index.jsx
+++ client/app/components/ApplicationArea/index.jsx
@@ -24,12 +24,7 @@ export default function ApplicationArea() {
         // if we see a javascript error on unexpected token where the unexpected token is '<', this usually means that a fallback html file (like index.html)
         // was served as content of script rather than the expected script, give a friendlier message in the console on what could be going on
         console.error(
-          i18next.t(
-            "ApplicationArea:[Uncaught SyntaxError: Unexpected token '<'] usually means that a fallback HTML file was returned from the server rather than the expected script. Check that the server is properly serving the file {{filename}}.",
-            {
-              filename: event.filename,
-            }
-          )
+          `[Uncaught SyntaxError: Unexpected token '<'] usually means that a fallback html file was returned from server rather than the expected script. Check that the server is properly serving the file ${event.filename}.`
         );
       }
       setUnhandledError(event.error);
