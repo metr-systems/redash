10b975249a5deaf67ee6ff3a9491e6d59cc40974
Imen Attatra <imen.attatra@gmail.com>
2024-11-25 15:02:58 +0100

    consider translation from .tsx files also

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   locales: ["en", "de"], // Languages you want to support
   output: "client/app/i18n/locales/$LOCALE/$NAMESPACE.json", // Where extracted files will be stored
-  input: ["./client/app/**/*.jsx"], // Files to parse (can be changed)
+  input: ["./client/app/**/*.{jsx,tsx}"], // Files to parse (can be changed)
   defaultNamespace: "common", // Default namespace
   keySeparator: ":", // Set to false if keys in code are dot-separated
   nsSeparator: false, // Set to false if namespaces in code are dot-separated
