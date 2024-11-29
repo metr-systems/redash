455c278c66581f28edf576f87907665f369ce88a
Imen Attatra <imen.attatra@gmail.com>
2024-11-29 15:49:28 +0100

    update test mock with language to considerand some special translations to consider

--- client/app/__tests__/mocks.js
+++ client/app/__tests__/mocks.js
@@ -5,10 +5,23 @@ const date = new Date("2000-01-01T02:00:00.000");
 MockDate.set(date);
 
 jest.mock("i18next", () => ({
-  t: key =>
-    key
-      .split(":")
-      .pop()
-      .trim(),
+  t: key => {
+    const translations = {
+      second_plural: "seconds",
+      minute_plural: "minutes",
+      hour_plural: "hours",
+      day_plural: "days",
+      week_plural: "weeks",
+      millisecond_plural: "milliseconds",
+    };
+    return (
+      translations[key] ||
+      key
+        .split(":")
+        .pop()
+        .trim()
+    );
+  },
   changeLanguage: jest.fn(),
+  language: "en",
 }));
