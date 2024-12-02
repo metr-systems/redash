97c1e5961fa6005d94ce75624279e549cd87666c
Imen Attatra <imen.attatra@gmail.com>
2024-12-02 15:03:11 +0100

    move ItemsList namespace to t const definition

--- client/app/components/items-list/components/EmptyState.jsx
+++ client/app/components/items-list/components/EmptyState.jsx
@@ -6,10 +6,10 @@ import BigMessage from "@/components/BigMessage";
 
 // Default "list empty" message for list pages
 export default function EmptyState(props) {
-  const { t } = useTranslation();
+  const { t } = useTranslation("ItemsList");
   return (
     <div className="text-center">
-      <BigMessage icon="fa-search" message={t("ItemsList:Sorry, we couldn't find anything.")} {...props} />
+      <BigMessage icon="fa-search" message={t("Sorry, we couldn't find anything.")} {...props} />
     </div>
   );
 }
