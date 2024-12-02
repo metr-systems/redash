7cc298cc8078815e60832cd6263dcabe6ed214c0
Imen Attatra <imen.attatra@gmail.com>
2024-12-02 14:06:13 +0100

    add translation for items list

--- client/app/components/items-list/components/LoadingState.jsx
+++ client/app/components/items-list/components/LoadingState.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+
+import { useTranslation } from "react-i18next";
+
 import BigMessage from "@/components/BigMessage";
 
 // Default "loading" message for list pages
 export default function LoadingState(props) {
+  const { t } = useTranslation();
   return (
     <div className="text-center">
-      <BigMessage icon="fa-spinner fa-2x fa-pulse" message="Loading..." {...props} />
+      <BigMessage icon="fa-spinner fa-2x fa-pulse" message={t("Loading...")} {...props} />
     </div>
   );
 }
