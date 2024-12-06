7c3d0c102a6694887394ebfcca336f2c10107c4a
Imen Attatra <imen.attatra@gmail.com>
2024-12-06 11:51:23 +0100

    Revert "update invite and reset emails to be in german"
    This reverts commit 2f4362a68dbf7b4c4917da3152bdb6dc3f4e56fc.

--- redash/authentication/account.py
+++ redash/authentication/account.py
@@ -65,7 +65,7 @@ def send_password_reset_email(user):
     context = dict(user=user, reset_link=reset_link)
     html_content = render_template("emails/reset.html", **context)
     text_content = render_template("emails/reset.txt", **context)
-    subject = _("Reset your password")
+    subject = "Reset your password"
 
     send_mail.delay([user.email], subject, html_content, text_content)
     return reset_link
