1ffcca24ceff201905132c9e5c2a9250ef7b9c77
Thiago Garcia da Silva <thiago.garciadasilva@metr.systems>
2025-05-16 10:33:33 +0200

    Use only the needed packages
    There's no need to use all the data sources. We only use the basic ones, plus google spreadsheet

--- publish_docker_images.sh
+++ publish_docker_images.sh
@@ -64,9 +64,7 @@ fi
 
 set -e
 
-
-docker build $DOCKER_CACHE_FLAG -t redash-metr .
-docker build $DOCKER_CACHE_FLAG --platform linux/amd64 -t redash-metr .
+docker build $DOCKER_CACHE_FLAG --build-arg install_groups="main,metr" --platform linux/amd64 -t redash-metr .
 
 
 function tag_and_push_image() {
