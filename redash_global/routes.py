f7e3b091f8ebf50f588c172fe44c81f13df77ba6
Imen Attatra <imen.attatra@gmail.com>
2026-04-08 15:38:55 +0200

    add auth routes (login, logout, admin)

--- redash_global/routes.py
+++ redash_global/routes.py
@@ -2,7 +2,11 @@
 
 from flask import Blueprint
 
+from redash_global.views.auth import admin_ui_view, login_page, logout_view
+
 # Create API blueprint
 api_blueprint = Blueprint("api", __name__, url_prefix="/global-api")
 
-# Register routes
+api_blueprint.add_url_rule("/admin/login", methods=["GET", "POST"], view_func=login_page, endpoint="login_page")
+api_blueprint.add_url_rule("/admin/logout", methods=["GET"], view_func=logout_view, endpoint="logout")
+api_blueprint.add_url_rule("/admin", methods=["GET"], view_func=admin_ui_view, endpoint="admin_ui")
