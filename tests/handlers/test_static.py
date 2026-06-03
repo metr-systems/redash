6dd9eb93e4205023ea238a96f4d4d9f8dc7fe9a3
Imen Attatra <imen.attatra@gmail.com>
2026-06-03 17:13:11 +0200

    fix: preserve query params on by_url_identifier redirect

--- tests/handlers/test_static.py
+++ tests/handlers/test_static.py
@@ -19,6 +19,28 @@ class TestDashboardByUrlIdentifier(BaseTestCase):
         expected_path = f"/{self.factory.org.slug}/dashboards/{dashboard.id}-{dashboard.slug}"
         self.assertEqual(rv.location, expected_path)
 
+    def test_redirect_preserves_query_params(self):
+        dashboard = self.factory.create_dashboard()
+        test_user = self.factory.create_user()
+
+        metr_dashboard = MetrDashboard(dashboard_id=dashboard.id, org_id=dashboard.org_id, url_identifier="test-id")
+        db.session.add(metr_dashboard)
+        db.session.commit()
+
+        rv = self.make_request(
+            "get",
+            "/dashboards/by_url_identifier/test-id?p_Liegenschaft=00000000-0000-0000-0000-000000000000",
+            user=test_user,
+            is_json=False,
+        )
+
+        self.assertEqual(rv.status_code, 302)
+        expected_path = (
+            f"/{self.factory.org.slug}/dashboards/{dashboard.id}-{dashboard.slug}"
+            "?p_Liegenschaft=00000000-0000-0000-0000-000000000000"
+        )
+        self.assertEqual(rv.location, expected_path)
+
     def test_not_found(self):
         # Create a user for authentication
         test_user = self.factory.create_user()
