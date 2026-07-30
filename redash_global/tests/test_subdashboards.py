724add52c840a99dccf6bc584ed48006429c4c05
Imen Attatra <imen.attatra@gmail.com>
2026-07-30 13:55:27 +0100

    Use factory pattern in the sub-dashboard tests
    also make sure to use f-string

--- redash_global/tests/test_subdashboards.py
+++ redash_global/tests/test_subdashboards.py
@@ -2,7 +2,6 @@ import datetime
 
 import pytest
 
-from redash.models import db
 from redash_global import settings
 
 URL = "/global-api/sub-dashboards"
@@ -10,17 +9,13 @@ URL = "/global-api/sub-dashboards"
 
 @pytest.fixture
 def template_org(factory):
-    org = factory.create_org(slug=settings.TEMPLATE_ORG_SLUG, name="Template")
-    db.session.commit()
-    return org
+    return factory.create_org(slug=settings.TEMPLATE_ORG_SLUG, name="Template")
 
 
 @pytest.fixture
 def create_sub_dashboard(factory, template_org):
     def create(name="Template A", **kwargs):
-        dashboard = factory.create_dashboard(org=template_org, name=name, **kwargs)
-        db.session.commit()
-        return dashboard
+        return factory.create_dashboard(org=template_org, name=name, **kwargs)
 
     return create
 
@@ -35,7 +30,6 @@ def test_requires_authentication(client):
 def test_list_returns_only_template_org_dashboards(admin_client, factory, create_sub_dashboard):
     create_sub_dashboard(name="In Template")
     factory.create_dashboard(org=factory.org, name="In Client Org")
-    db.session.commit()
 
     data = admin_client.get(URL).get_json()
 
@@ -54,11 +48,8 @@ def test_list_excludes_drafts_and_archived(admin_client, create_sub_dashboard):
 
 
 def test_list_orders_newest_first(admin_client, create_sub_dashboard):
-    older = create_sub_dashboard(name="Older")
-    newer = create_sub_dashboard(name="Newer")
-    older.created_at = datetime.datetime(2020, 1, 1)
-    newer.created_at = datetime.datetime(2021, 1, 1)
-    db.session.commit()
+    create_sub_dashboard(name="Older", created_at=datetime.datetime(2020, 1, 1))
+    create_sub_dashboard(name="Newer", created_at=datetime.datetime(2021, 1, 1))
 
     data = admin_client.get(URL).get_json()
 
@@ -67,9 +58,9 @@ def test_list_orders_newest_first(admin_client, create_sub_dashboard):
 
 def test_list_paginates(admin_client, create_sub_dashboard):
     for i in range(3):
-        create_sub_dashboard(name="D{}".format(i))
+        create_sub_dashboard(name=f"D{i}")
 
-    data = admin_client.get("{}?page=1&page_size=2".format(URL)).get_json()
+    data = admin_client.get(f"{URL}?page=1&page_size=2").get_json()
 
     assert data["count"] == 3
     assert len(data["results"]) == 2
@@ -81,5 +72,5 @@ def test_result_includes_link_out_url(admin_client, create_sub_dashboard):
     data = admin_client.get(URL).get_json()
 
     assert data["results"][0]["url"].endswith(
-        "/{}/dashboards/{}-{}".format(settings.TEMPLATE_ORG_SLUG, dashboard.id, dashboard.slug)
+        f"/{settings.TEMPLATE_ORG_SLUG}/dashboards/{dashboard.id}-{dashboard.slug}"
     )
